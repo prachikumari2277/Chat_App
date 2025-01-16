5b78e890cf2c4bae73f22173c93c1b4ac32d819a
prachikumari2277 <162799003+prachikumari2277@users.noreply.github.com>
2025-01-16 11:20:06 +0530

    Update messages.js

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,9 +1,11 @@
-const { addMessage, getMessages , clearChat } = require("../controllers/messageController");
+const { addMessage, getMessages , clearChat, sendPhoto , getPhoto} = require("../controllers/messageController");
 const router = require("express").Router();
 
 router.post("/addmsg/", addMessage);
 router.post("/getmsg/", getMessages);
 router.post("/clearMessage/", clearChat);
+router.post("/sendPhoto/", sendPhoto);
+router.post("/getPhoto/", getPhoto);
+
 
 module.exports = router;
-
